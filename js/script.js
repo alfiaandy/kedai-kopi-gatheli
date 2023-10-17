706e62ce62a7e47ac4eb68476a7c699759ed7404
pukeyss <alfiandwiyanto7@gmail.com>
2023-10-18 00:02:59 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// Toggle class active
+// Toggle class active untuk coffee menu
 const navbarNav = document.querySelector('.navbar-nav');
-//  Keika coffee menu di klik
+//  Ketika coffee menu di klik
 document.querySelector('#coffee-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-// klik diluar side bar untuk menghilangkan nav
-const coffee = document.querySelector('#coffee-menu');
+// ketika search button di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+
+// klik diluar elemen
+const cm = document.querySelector('#coffee-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function (e){
-    if (!coffee.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!cm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 });
